f76527fc39fb533065cf873a2944c0e1182019dc
dannyunico <danielbermudezreyes@gmail.com>
2020-02-13 11:32:57 -0400

    questions

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -6,3 +6,38 @@ $(window).scroll(function () {
     $('.navbar-fixed-js').removeClass('fixed');
   }
 });
+
+
+$('.slider-preguntas').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+
+  responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    }
+  ]
+});
